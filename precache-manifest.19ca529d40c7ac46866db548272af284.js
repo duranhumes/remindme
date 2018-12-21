62304f34d04fdf58afa943d7a0b05871bd579f4d
duranhumes <duranhumes@gmail.com>
2018-12-21 16:35:59 -0500

    Updates

---
+++ precache-manifest.19ca529d40c7ac46866db548272af284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c0940dc8fcf915515f",
+    "url": "/remindme/static/css/main.aaf42b46.chunk.css"
+  },
+  {
+    "revision": "05c0940dc8fcf915515f",
+    "url": "/remindme/static/js/main.05c0940d.chunk.js"
+  },
+  {
+    "revision": "0516bf7d008f7ccef063",
+    "url": "/remindme/static/js/1.0516bf7d.chunk.js"
+  },
+  {
+    "revision": "afd8afc27602a16056a9",
+    "url": "/remindme/static/js/runtime~main.afd8afc2.js"
+  },
+  {
+    "revision": "e66e1266d13b8c1d885b953176886b34",
+    "url": "/remindme/index.html"
+  }
+];
